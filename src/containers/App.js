8d6c29cbd65f21dd5aa4c3463740120076ca08d4
FaithChange <faith333change@gmail.com>
2023-02-28 01:45:13 -0800

    fix read me file & app component

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,15 +29,15 @@ class App extends Component {
             return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
         return !robots.length ?
-        <h1>Loading</h1>  : 
-        <div className="tc">
-            <h1 className="f2">RobotFriends</h1>
-            <SearchBox searchChange={this.onSearchChange} />
-            <Scroll>
-                <CardList robots={filteredRobots} />
-            </Scroll>
-        </div>
-    
+            <h1>Loading</h1> :
+            <div className="tc">
+                <h1 className="f2">RobotFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
+            </div>
+
     
             
         
